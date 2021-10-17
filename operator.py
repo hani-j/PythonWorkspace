db44ceac58f95dea49027fccb2a0fba24a1396b6
hani <gkdms4248@gmail.com>
2021-10-17 21:19:56 +0900

    operator2

--- operator.py
+++ operator.py
@@ -3,10 +3,10 @@ print(3-2) # 1
 print(5*2) # 10
 print(6/3) # 2
 
-print(2**3) # 2^3 = 8
+print(2**3) # 2^3 = 8 2의 / 세제곱
 print(5%3) # 나머지 구하기 2
 print(10%3) # 1
-print(5//3) # 1
+print(5//3) # 1 / 몫
 print(10//3) # 3
 
 print(10 > 3) # True
