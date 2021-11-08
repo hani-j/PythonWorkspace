7fd045c356c7feefdb6a5f412b1eb994fe284582
hani <gkdms4248@gmail.com>
2021-11-08 23:45:16 +0900

    inoutput

---
+++ 8_inoutput/01_standard.py
@@ -0,0 +1,6 @@
+print("Python", "Java") # Python Java
+print("Python" + "Java") # PythonJava
+print("Python", "Java", sep=",") # Python,Java
+print("Python", "Java", "JavaScript", sep=" vs ") # Python vs Java vs JavaScript
+print("Python", "Java", sep=",", end="?")
+print("무엇이 더 재밌을까요?") # Python,Java?무엇이 더 재밌을까요?
