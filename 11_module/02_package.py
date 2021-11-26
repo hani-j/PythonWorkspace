1f3ff3683c4e54652eade037935840c0df4338cf
hani <gkdms4248@gmail.com>
2021-11-26 13:36:45 +0900

    finish

--- 11_module/02_package.py
+++ 11_module/02_package.py
@@ -1,1 +1,22 @@
 # 여러 모듈 파일을 묶어둔 것
+
+# 방법 1
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# 방법 2
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+from travel import *
+
+import inspect
+import random
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
---
+++ 11_module/03__all__.py
@@ -0,0 +1,9 @@
+# from random import *
+# from travel import *
+
+# * => __init__ 에 __all__ 정의
+from travel import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
