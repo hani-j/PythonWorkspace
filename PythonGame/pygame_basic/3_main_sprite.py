242d24f33e61c1d2f089bae3eac848e9c4f5fc4c
hani <gkdms4248@gmail.com>
2021-11-29 22:59:10 +0900

    character

--- PythonGame/pygame_basic/3_main_sprite.py
+++ PythonGame/pygame_basic/3_main_sprite.py
@@ -13,6 +13,14 @@ pygame.display.set_caption("Nado Game") #게임 이름
 # 배경 이미지 불러오기
 background = pygame.image.load("C:/Users/gkdms/Desktop/코딩/Python/PythonWorkspace/PythonGame/pygame_basic/background.png")
 
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("C:/Users/gkdms/Desktop/코딩/Python/PythonWorkspace/PythonGame/pygame_basic/character.png")
+character_size = character.get_rect().size # rectangle 이미지의 크기를 구해옴
+character_width = character_size[0] # 캐릭터의 가로 크기
+character_height = character_size[1] # 캐릭터의 세로 크기
+character_x_pos = screen_width / 2 - character_width # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로)
+character_y_pos = screen_height - character_height# 화면 세로 크기 가장 아래에 해당하는 곳에 위치 (세로)
+
 # 이벤트 루프
 running = True # 게임이 진행중인가?
 while running:
@@ -20,8 +28,9 @@ while running:
 		if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
 			running = False # 게임이 진행중이 아님
 
-	# screen.fill((0, 0, 255))
 	screen.blit(background, (0, 0))
+
+	screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
 
 	# 매 프레임마다 화면을 새로 그려주는 동작을 해줘야 함
 	pygame.display.update() # 게임화면을 다시 그리기!
