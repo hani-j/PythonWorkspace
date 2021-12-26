5ba7ac477c18c57c37efaf807c37be5df0eb1daa
hani <gkdms4248@gmail.com>
2021-12-26 19:49:58 +0900

    basic 41-45

---
+++ PythonCT/basic42.py
@@ -0,0 +1,4 @@
+a = float(input())
+print(format(a, ".2f"))
+
+# format(a, ".2f") : a를 소수점 아래 3번째 자리에서 반올림 한 값
---
+++ PythonCT/basic45.py
@@ -0,0 +1,6 @@
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+p = (a+b+c) / 3
+print(a+b+c, format(p,".2f"))
