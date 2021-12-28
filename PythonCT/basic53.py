4ea23bf8ec985beefb03632055fbd0f1daa2c5a6
hani <gkdms4248@gmail.com>
2021-12-28 14:36:35 +0900

    basic 51-55

---
+++ PythonCT/basic53.py
@@ -0,0 +1,5 @@
+a = bool(int(input()))
+print(not a)
+
+# not True, not False, not a
+# 논리값을 역으로
---
+++ PythonCT/basic54.py
@@ -0,0 +1,4 @@
+a, b = input().split()
+print(bool(int(a)) and bool(int(b)))
+
+# and : 둘다 True => True , 나머지 False
